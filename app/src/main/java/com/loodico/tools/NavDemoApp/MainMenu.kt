47dc71d2b32075f94e804b3cb00f1f2d9b6e8743
josejoserojas-tomtom <loodico@gmail.com>
2022-11-15 14:29:41 +0100

    added the activities for the domains demo code

--- app/src/main/java/com/loodico/tools/NavDemoApp/MainMenu.kt
+++ app/src/main/java/com/loodico/tools/NavDemoApp/MainMenu.kt
@@ -15,4 +15,29 @@ class MainMenu : AppCompatActivity() {
         val myIntent = Intent(this@MainMenu, BasicNavActivity::class.java)
         this@MainMenu.startActivity(myIntent)
     }
+
+    fun launchMapDemo(button : android.view.View) {
+        val myIntent = Intent(this@MainMenu, MapDemoActivity::class.java)
+        this@MainMenu.startActivity(myIntent)
+    }
+
+    fun launchNavigationDemo(button : android.view.View) {
+        val myIntent = Intent(this@MainMenu, NavigationDemoActivity::class.java)
+        this@MainMenu.startActivity(myIntent)
+    }
+
+    fun launchOfflineMapsDemo(button : android.view.View) {
+        val myIntent = Intent(this@MainMenu, OfflineMapsDemoActivity::class.java)
+        this@MainMenu.startActivity(myIntent)
+    }
+
+    fun launchRoutingDemo(button : android.view.View) {
+        val myIntent = Intent(this@MainMenu, RoutingDemoActivity::class.java)
+        this@MainMenu.startActivity(myIntent)
+    }
+
+    fun launchSearchDemo(button : android.view.View) {
+        val myIntent = Intent(this@MainMenu, SearchDemoActivity::class.java)
+        this@MainMenu.startActivity(myIntent)
+    }
 }
